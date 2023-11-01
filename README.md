8d7d027c084b5e1562ec61e0836d5113a15037d6
Philip Paetz <philip.paetz@me.com>
2023-11-01 17:07:32 +0100

    feat(cStable volume): added volume snapshot

--- README.md
+++ README.md
@@ -8,20 +8,51 @@ TBD
 
 ## 2. cStables Volume
 
-TBD
+The total cStable volume (cUSD + cEUR + cREAL) across a 1-year timeframe.
+
+Volume is defined as all cStable transfers **from** an address + all cStable transfers **to** an address
+
+- [Dune Query that was used to export final snapshot CSV](https://dune.com/queries/3163689/5279843)
+- [cStable Volume Snapshot](./src/snapshots/cstable-volume/cstable-volume-snapshot.csv)
+
+### Calculation Method
+
+- Take 1 snapshot of the total cStables volume per address over a 1-year timeframe from 15.11.2022 12:00 pm UTC to 15.10.2023 12:00 pm UTC
+- Sum up volume across all cStables: `cUSD volume + cEUR volume + cREAL volume`
+- Denominate volume in USD using the average exchange rate over 365 days between 15.11.2022 and 15.10.2023
+- Filter out addresses with less than $10 of total volume
+
+### Example
+
+- Bob has sent 10 cUSD at day 1 of the snapshot period
+- Bob received 5 cUSD at day 100 of the snapshot period
+- Bob has send 10 cEUR at day 365 of the snapshot period
+- The average cEUR/USD exchange rate over the 365 days snapshot period was 1.05
+- Bob has a total volume of: 10 cUSD (spent) + 5 cUSD (received) + (10 cEUR * 1.05 exchange rate = 10.5 cUSD) = $25.5 total volume
 
 ## 3. cStables Balances
 
 The average cStable balance (cUSD/cEUR/cREAL) denominated in USD across a 1-year timeframe.
 
-- [cStables Balances Snapshot](./src/snapshots/cstable-balances/total-average-across-all-snapshots-excluding-validators.csv)
+- [cStable Balances Snapshot](./src/snapshots/cstable-balances/total-average-across-all-snapshots-excluding-validators.csv)
 - [Dune Query that was used to export 12 snapshot CSVs at different dates](https://dune.com/queries/3144937/5269961)
 
 ### Calculation Method
 
-- We took 12 monthly snapshots, 1 per month from November 15 2022 to October 15 2023, of addresses who held any cStables on the date of the snapshot
+- Take 12 monthly snapshots, 1 per month from November 15 2022 to October 15 2023, of addresses who held any cStables on the date of the snapshot
 - Minimum cUSD OR cEUR OR cREAL balance must have been > 10 to be included in the monthly snapshots
 - Converted the cStables balances into USD using the exchange rate at the time of the respective monthly snapshot
 - Calculated the average cStable balance in USD over all 12 monthly snapshots
 - Filtered out addresses with less than $10 in average cStable balances across all snapshots
 - Filtered out validator addresses (because for technical reasons their cUSD balances can't be correctly calculated by indexers, concretely the block rewards paid in cUSD to validators do not emit `Transfer` events that indexers pick up)
+
+### Example
+
+- Prior to snapshot 1 (15.11.2022 12pm UTC), Alice had received 55 cUSD and spent 5 cUSD
+- Therefore, Alice's balance at snapshot 1 was 50 cUSD (55 - 5)
+- She doesn't send or receive any further transactions until one day before snapshot 12
+- One day before snapshot 12, Alice receives 100 cEUR
+- On the day of snapshot 12, cEUR had an exchange rate of 1.06 USD, so Alice's 100 cEUR were valued at $106 USD
+- Her total average cStable balance across all 12 snapshots would be
+  - ($50 (cUSD balance) + $106 (cEUR balance)) / 12 snapshots = **$13 average combined cStable balance**
+  - Alice would be eligible for the airgrab because she has more than $10 in average cStable value across all snapshots
---
+++ sql-queries/cstable-volume-snapshots.sql
@@ -0,0 +1,254 @@
+WITH
+    --
+    --
+    -- Prices
+    --
+    -- Combine cUSD, cEUR, and BRL prices in one table 
+    average_prices AS (
+        SELECT
+            symbol,
+            AVG(price) average_price
+        FROM
+            prices.usd
+        WHERE
+            blockchain = 'celo'
+            AND contract_address IN (
+                -- cUSD https://celoscan.io/address/0x765DE816845861e75A25fCA122bb6898B8B1282a
+                0x765DE816845861e75A25fCA122bb6898B8B1282a,
+                -- cEUR https://celoscan.io/address/0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73
+                0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73
+            )
+            -- We want the average price within the snapshot timeframe
+            AND minute >= date_trunc ('minute', TIMESTAMP '{{from}}')
+            AND minute <= date_trunc ('minute', TIMESTAMP '{{to}}')
+        GROUP BY
+            symbol,
+            contract_address
+        UNION ALL
+        --
+        -- There's no easily accessible cREAL price feed in Dune, so we use a chainlink
+        -- price feed to get BRL prices and get them into the prior table via UNION ALL
+        SELECT
+            base as symbol, -- Match schema of prices.usd table
+            AVG(oracle_price) price
+        FROM
+            chainlink.price_feeds
+        WHERE
+            base = 'BRL'
+            AND quote = 'USD'
+            AND block_date >= DATE (TIMESTAMP '{{from}}')
+            AND block_date <= DATE (TIMESTAMP '{{to}}')
+        GROUP BY
+            base
+    ),
+    --
+    --
+    -- cUSD
+    --
+    -- Get all received cUSD
+    cUSD_inflow AS (
+        SELECT
+            to AS address,
+            COALESCE(SUM(value), CAST(0 AS uint256)) AS total
+        FROM
+            mento_celo.StableToken_evt_Transfer
+        WHERE
+            -- Filter out 0 address
+            to != 0x0000000000000000000000000000000000000000
+            AND evt_block_time >= TIMESTAMP '{{from}}'
+            AND evt_block_time <= TIMESTAMP '{{to}}'
+        GROUP BY
+            to
+    ),
+    -- Get all spent cUSD
+    cUSD_outflow AS (
+        SELECT
+            "from" AS address,
+            COALESCE(SUM(value), CAST(0 AS uint256)) AS total
+        FROM
+            mento_celo.StableToken_evt_Transfer
+        WHERE
+            evt_block_time >= TIMESTAMP '{{from}}'
+            AND evt_block_time <= TIMESTAMP '{{to}}'
+        GROUP BY
+            "from"
+    ),
+    -- Calculate total cUSD volume: `inflow + outflow = volume`
+    cUSD_volume as (
+        SELECT
+            inflow.address AS address,
+            (inflow.total + outflow.total) / 1e18 AS volume,
+            ((inflow.total + outflow.total) / 1e18) * (
+                SELECT
+                    average_price
+                FROM
+                    average_prices
+                WHERE
+                    symbol = 'cUSD'
+            ) AS volume_in_usd
+        FROM
+            cUSD_inflow AS inflow
+            LEFT OUTER JOIN cUSD_outflow AS outflow ON inflow.address = outflow.address
+        WHERE
+            (inflow.total + outflow.total) / 1e18 >= CAST(10 as double)
+    ),
+    --
+    --
+    -- cEUR
+    --
+    -- Get all received cEUR
+    cEUR_inflow AS (
+        SELECT
+            to AS address,
+            COALESCE(SUM(value), CAST(0 AS uint256)) AS total
+        FROM
+            mento_celo.StableTokenEUR_evt_Transfer
+        WHERE
+            -- Filter out 0 address
+            to != 0x0000000000000000000000000000000000000000
+            AND evt_block_time >= TIMESTAMP '{{from}}'
+            AND evt_block_time <= TIMESTAMP '{{to}}'
+        GROUP BY
+            to
+    ),
+    -- Get all spent cEUR
+    cEUR_outflow AS (
+        SELECT
+            "from" AS address,
+            COALESCE(SUM(value), CAST(0 AS uint256)) AS total
+        FROM
+            mento_celo.StableTokenEUR_evt_Transfer
+        WHERE
+            evt_block_time >= TIMESTAMP '{{from}}'
+            AND evt_block_time <= TIMESTAMP '{{to}}'
+        GROUP BY
+            "from"
+    ),
+    -- Calculate total cEUR volume: `inflow + outflow = volume`
+    cEUR_volume as (
+        SELECT
+            inflow.address AS address,
+            (inflow.total + outflow.total) / 1e18 AS volume,
+            ((inflow.total + outflow.total) / 1e18) * (
+                SELECT
+                    average_price
+                FROM
+                    average_prices
+                WHERE
+                    symbol = 'cEUR'
+            ) AS volume_in_usd
+        FROM
+            cEUR_inflow AS inflow
+            LEFT OUTER JOIN cEUR_outflow AS outflow ON inflow.address = outflow.address
+        WHERE
+            (inflow.total + outflow.total) / 1e18 >= CAST(10 as double)
+    ),
+    --
+    --
+    -- cREAL
+    --
+    -- Get all received cREAL
+    cREAL_inflow AS (
+        SELECT
+            to AS address,
+            COALESCE(SUM(value), CAST(0 AS uint256)) AS total
+        FROM
+            mento_celo.StableTokenBRL_evt_Transfer
+        WHERE
+            -- Filter out 0 address
+            to != 0x0000000000000000000000000000000000000000
+            AND evt_block_time >= TIMESTAMP '{{from}}'
+            AND evt_block_time <= TIMESTAMP '{{to}}'
+        GROUP BY
+            to
+    ),
+    -- Get all spent cREAL
+    cREAL_outflow AS (
+        SELECT
+            "from" AS address,
+            COALESCE(SUM(value), CAST(0 AS uint256)) AS total
+        FROM
+            mento_celo.StableTokenBRL_evt_Transfer
+        WHERE
+            evt_block_time >= TIMESTAMP '{{from}}'
+            AND evt_block_time <= TIMESTAMP '{{to}}'
+        GROUP BY
+            "from"
+    ),
+    -- Calculate total cREAL volume: `inflow + outflow = volume`
+    cREAL_volume as (
+        SELECT
+            inflow.address AS address,
+            (inflow.total + outflow.total) / 1e18 AS volume,
+            ((inflow.total + outflow.total) / 1e18) * (
+                SELECT
+                    average_price
+                FROM
+                    average_prices
+                WHERE
+                    symbol = 'BRL'
+            ) AS volume_in_usd
+        FROM
+            cREAL_inflow AS inflow
+            LEFT OUTER JOIN cREAL_outflow AS outflow ON inflow.address = outflow.address
+        WHERE
+            (inflow.total + outflow.total) / 1e18 >= CAST(10 as double)
+    )
+    --
+    --
+    -- Combine total volumes into final output table
+    --
+SELECT
+    COALESCE(cUSD.address, cEUR.address, cREAL.address) "Address",
+    COALESCE(cUSD.volume_in_usd, 0) + COALESCE(cEUR.volume_in_usd, 0) + COALESCE(cREAL.volume_in_usd, 0) "Total Volume in USD",
+    cUSD.volume_in_usd "cUSD Volume in USD",
+    cEUR.volume_in_usd "cEUR Volume in USD",
+    cREAL.volume_in_usd "cREAL Volume in USD",
+    -- including a column with address metadata for contract addresses to simplify manual snapshot review
+    CASE
+    -- If there's a contract creation TX hash for this address, it means it should be contract (and not an EOA)
+        WHEN creation_trace.tx_hash IS NOT NULL THEN (
+            CASE
+            -- If the address is on the safe_celo.safes Dune table, label it as 'Gnosis Safe'
+                WHEN gnosis_safe.tx_hash IS NOT NULL THEN 'Gnosis Safe'
+                -- else if Dune has a contract name (available for many verified contracts), use the contract name.
+                -- (annoyingly, some contracts have more than 1 name which is why this complicated merging of contract names into 1 column is required)
+                WHEN array_join (array_agg (contract.name), ', ') != '' THEN array_join (array_agg (contract.name), ', ')
+                -- else tag it as 'unverified'
+                ELSE 'unverified'
+            END
+        )
+        ELSE NULL
+    END AS "Contract",
+    cUSD.volume "cUSD Volume",
+    cEUR.volume "cEUR Volume",
+    cREAL.volume "cREAL Volume"
+FROM
+    cUSD_volume cUSD
+    FULL OUTER JOIN cEUR_volume cEUR ON cUSD.address = cEUR.address
+    FULL OUTER JOIN cREAL_volume cREAL ON (
+        cEUR.address = cREAL.address
+        OR cUSD.address = cREAL.address
+    )
+    LEFT JOIN celo.contracts "contract" ON contract.address = COALESCE(cUSD.address, cEUR.address, cREAL.address)
+    LEFT JOIN celo.creation_traces "creation_trace" ON creation_trace.address = COALESCE(cUSD.address, cEUR.address, cREAL.address)
+    LEFT JOIN safe_celo.safes "gnosis_safe" ON gnosis_safe.address = COALESCE(cUSD.address, cEUR.address, cREAL.address)
+WHERE
+    -- min. $10 USD total volume to qualify
+    COALESCE(cUSD.volume_in_usd, 0) + COALESCE(cEUR.volume_in_usd, 0) + COALESCE(cREAL.volume_in_usd, 0) > 10
+    --
+    -- This group-by clause is necessary because 1 address can possibly have multiple contract names on
+    -- the celo.contracts table which would lead to multiple rows for the same address potentially leading
+    -- to double-counting for eligibility.
+GROUP BY
+    COALESCE(cUSD.address, cEUR.address, cREAL.address),
+    creation_trace.tx_hash,
+    gnosis_safe.tx_hash,
+    cUSD.volume,
+    cUSD.volume_in_usd,
+    cEUR.volume,
+    cEUR.volume_in_usd,
+    cREAL.volume,
+    cREAL.volume_in_usd
+ORDER BY
+    COALESCE(cUSD.volume_in_usd, 0) + COALESCE(cEUR.volume_in_usd, 0) + COALESCE(cREAL.volume_in_usd, 0) DESC
