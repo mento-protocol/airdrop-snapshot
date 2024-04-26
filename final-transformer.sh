e294c0e67404229eb711e654e98489e29dbffd6b
Philip Paetz <philip.paetz@me.com>
2024-04-26 14:24:03 +0200

    feat: add wei transformation to final transformer

--- final-transformer.sh
+++ final-transformer.sh
@@ -5,8 +5,11 @@ cut -d, -f1,5 final-snapshots/airdrop-amounts-per-address.csv >buffer.csv
 # Sorts by allocation descending
 sort -t, -k2,2 -rn buffer.csv -o buffer.csv
 
-# Cuts header (which is the last line after the sort operation)
-sed '$d' buffer.csv >final-snapshots/final-allocation.csv
+# Multiplies the 2nd column by 1e18 to convert to wei
+awk -F',' '{ $2 *= 1e18; print }' buffer.csv >buffer-wei.csv
 
-# Removes the temp buffer file
-rm buffer.csv
+# Cuts header (which is the last line after the sort operation)
+sed '$d' buffer-wei.csv >final-snapshots/final-allocations-in-wei.csv
+
+# Removes the temp buffer files
+rm buffer.csv buffer-wei.csv
