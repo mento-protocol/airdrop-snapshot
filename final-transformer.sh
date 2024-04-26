44b411c987fdea2d23679ddb89f4f145196be243
Philip Paetz <philip.paetz@me.com>
2024-04-26 15:26:35 +0200

    fix: missing commas in final output

--- final-transformer.sh
+++ final-transformer.sh
@@ -6,7 +6,7 @@ cut -d, -f1,5 final-snapshots/airdrop-amounts-per-address.csv >buffer.csv
 sort -t, -k2,2 -rn buffer.csv -o buffer.csv
 
 # Multiplies the 2nd column by 1e18 to convert to wei
-awk -F',' '{ $2 *= 1e18; print }' buffer.csv >buffer-wei.csv
+awk -F',' 'BEGIN{OFS=","} { $2 *= 1e18; print }' buffer.csv >buffer-wei.csv
 
 # Cuts header (which is the last line after the sort operation)
 sed '$d' buffer-wei.csv >final-snapshots/final-allocations-in-wei.csv
