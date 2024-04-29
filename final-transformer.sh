e9301ddbcce67d188e2da3a465a0514be255fd39
chapati <philip.paetz@me.com>
2024-04-29 15:28:42 +0200

    feat: add final transformer (#12)

---
+++ final-transformer.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# Removes all columns except for address and total allocation
+cut -d, -f1,5 final-snapshots/airdrop-amounts-per-address.csv >buffer.csv
+
+# Sorts by allocation descending
+sort -t, -k2,2 -rn buffer.csv -o buffer.csv
+
+# Multiplies the 2nd column by 1e18 to convert to wei
+awk -F',' 'BEGIN{OFS=","} { $2 *= 1e18; print }' buffer.csv >buffer-wei.csv
+
+# Cuts header (which is the last line after the sort operation)
+sed '$d' buffer-wei.csv >final-snapshots/final-allocations-in-wei.csv
+
+# Removes the temp buffer files
+rm buffer.csv buffer-wei.csv
